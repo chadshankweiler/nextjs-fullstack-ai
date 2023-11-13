6b0c60df79cf680f00e875809f3ec5a5631526b6
chadshankweiler <chadshankweiler@gmail.com>
2023-11-12 21:28:07 -1000

    Modified auth redirect for new users from /new-user to /journal

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,9 +1,33 @@
-import { UserButton } from "@clerk/nextjs";
+import { prisma } from "@/utils/db";
+import { UserButton, currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
-const NewUser = () => {
+const createNewUser = async () => {
+    const user = await currentUser();
+    console.log(user);
+    const match = await prisma.user.findUnique({
+        where: {
+            clerkId: user.id as String,
+        },
+    });
+
+    if (!match) {
+        await prisma.user.create({
+            data: {
+                clerkId: user.id,
+                email: user?.emailAddresses[0].emailAddress
+            },
+        });
+    }
+
+    redirect("/journal");
+};
+
+const NewUser = async () => {
+    await createNewUser();
     return (
         <div>
-            <UserButton afterSignOutUrl="127.0.0.1:3000"/>
+            <UserButton afterSignOutUrl="127.0.0.1:3000" />
         </div>
     );
 };
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
+import { auth } from "@clerk/nextjs";
 import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+    const { user } = await auth()
+    let href = user ? "/journal" : "/new-user"
     return (
         <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
             <div className="w-full max-w-[600px]">
@@ -9,7 +12,7 @@ export default function Home() {
                     App for tracking mood with every journal entry.
                 </p>
                 <div>
-                    <Link href="/journal">
+                    <Link href={href}>
                         <button className="bg-blue-600 px-4 py-2 capitalize rounded-lg text-lg">
                             get started
                         </button>
